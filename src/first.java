f24494785392e8a9b0ca9be0f6a256693bc428b7
zuiyanyu <18838971944@163.com>
2019-10-07 11:21:38 +0800

    调整样式

--- src/first.java
+++ src/first.java
@@ -1,7 +1,8 @@
 class Hello{
     public static void main(String[] args){
         int a = 1 ;
-	int b =a + 1;
-	System.out.println("hellow") ;
+        int b =a + 1;
+
+        System.out.println("hellow") ;
     }
 }
